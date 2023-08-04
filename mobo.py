8608a54064a0cceef5acd849eb46f8059ab9b8f9
Mjx <599939317@qq.com>
2023-08-04 21:12:32 +0800

    update

--- mobo.py
+++ mobo.py
@@ -1,16 +1,20 @@
-from sklearn.linear_model import LinearRegression
-from Bgolearn import BGOsampling
+
+
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.gaussian_process.kernels import RBF
 from sklearn.gaussian_process import GaussianProcessRegressor
+from tabulate import tabulate
 
 import pandas as pd
-import cmu
 from typing import Optional
+import numpy as np
+import warnings
+
+warnings.filterwarnings('ignore')
 
 
-class Mobo(object):
+class Mobo4mat:
     """
     Multi Objective Bayesian Optimization.
 
@@ -24,19 +28,13 @@ class Mobo(object):
     Examples
     --------
     """
-    def __init__(self):
-        pass
-
-    def fit(self, X, y, visual_data, mission: Optional[str]=None, method='HV', number=1, objective: Optional[str]=None, ref_point=Optional[list]=None):
+    def fit(self, X, y, visual_data, method, number, objective, ref_point):
         Xtrain = X
         Ytrain = y
-        Xtest=visual_data 
-        target_names = Ytrain.columns.tolist()
-
-        if objective == 'max':
-            pareto_front = find_non_dominated_solutions(Xtrain, target_names)
-            pareto_front = pd.DataFrame(pareto_front, columns=target_names)
-
+        Xtest = visual_data 
+        target_name = Ytrain.columns.tolist()
+        feature_name = Xtrain.columns.tolist()
+        if objective == 'min':
             kernel = RBF(length_scale=1.0)
             gp_model = GaussianProcessRegressor(kernel=kernel)
             gp_model.fit(Xtrain, Ytrain)
@@ -48,8 +46,8 @@ class Mobo(object):
                     for i in range(Ypred.shape[0]):
                         i_Ypred = Ypred.iloc[i]
                         Ytrain_i_Ypred = Ytrain.append(i_Ypred)
-                        i_pareto_front = find_non_dominated_solutions(Ytrain_i_Ypred.values, Ytrain_i_Ypred.columns.tolist())
-                        i_HV_value = dominated_hypervolume(i_pareto_front, ref_point)
+                        i_pareto_front = self.find_non_dominated_solutions(Ytrain_i_Ypred.values, Ytrain_i_Ypred.columns.tolist())
+                        i_HV_value = self.dominated_hypervolume(i_pareto_front, ref_point)
                         HV_values.append(i_HV_value)
                     
                     HV_values = pd.DataFrame(HV_values, columns=['HV values'])
@@ -57,13 +55,15 @@ class Mobo(object):
 
                     max_idx = HV_values.nlargest(number, 'HV values').index
                     recommend_point = Xtest.loc[max_idx]
-                    Xtest = Xtest.drop(max_idx)
-                    print('The maximum value of HV:', HV_values.loc[max_idx]) 
-                    print('The recommended point is :', recommend_point)
+                    # Xtest = Xtest.drop(max_idx)
+                    print('The maximum value of HV: \n ', tabulate(HV_values.loc[max_idx].values))
+                    print('The recommended point is :\n', tabulate(recommend_point.values, headers = feature_name+target_name, tablefmt = 'pretty'))
             elif method == 'EHVI':
                 pass
-            
-    def preprocess(data, target_number, normalize: Optional[str]=None):
+        elif objective == 'max':
+            pass
+
+    def preprocess(self, data, target_number, normalize: Optional[str]=None):
         df = pd.read_csv(data)
         X = df.iloc[:,:-target_number].values
         y = df.iloc[:,-target_number:]
@@ -75,5 +75,68 @@ class Mobo(object):
             X = scaler.fit_transform(X)
         else:
             X = X        
-
         return X, y
+
+    def non_dominated_sorting(self, fitness_values): # min
+        num_solutions = fitness_values.shape[0]
+        domination_counts = np.zeros(num_solutions, dtype=int)  
+        dominated_solutions = [[] for _ in range(num_solutions)]  
+        frontiers = []  
+
+        for i in range(num_solutions): 
+            for j in range(i + 1, num_solutions):  
+                if np.all(fitness_values[i,:] <= fitness_values[j,:]): 
+                    if np.any(fitness_values[i,:] < fitness_values[j,:]):  
+                        domination_counts[j] += 1  
+                    else:
+                        dominated_solutions[i].append(j)  
+                elif np.all(fitness_values[i,:] >= fitness_values[j,:]):  
+                    if np.any(fitness_values[i,:] > fitness_values[j,:]):  
+                        domination_counts[i] += 1 
+                    else:
+                        dominated_solutions[j].append(i) 
+
+            if domination_counts[i] == 0: 
+                frontiers.append(i)  
+
+                i = 0
+                while i < len(frontiers):  
+                    current_frontier = frontiers[i]
+                    next_frontier = []
+                    for j in dominated_solutions[current_frontier]:  
+                        domination_counts[j] -= 1  
+                        if domination_counts[j] == 0:  
+                            next_frontier.append(j)  
+                    i += 1
+                    frontiers.extend(next_frontier)  
+        return frontiers
+    
+
+    def find_non_dominated_solutions(self, fitness_values, feature_name):
+        frontiers = self.non_dominated_sorting(fitness_values)
+        # non_dominated_solutions_idx = []
+        # for frontier in frontiers:
+        #     non_dominated_solutions_idx.extend(frontier)
+
+        non_dominated_solutions_Data = fitness_values[frontiers]
+        non_dominated_solutions_Data = pd.DataFrame(non_dominated_solutions_Data, columns=feature_name)
+        non_dominated_solutions_Data.sort_values(by=feature_name[0], inplace=True)
+
+        return  non_dominated_solutions_Data
+
+    def dominated_hypervolume(self, pareto_data, ref_point):
+        pareto_data = np.vstack([pareto_data, ref_point])
+        pareto_data = pareto_data[np.argsort(-pareto_data[:,0])]
+        S = 0
+        for i in range(pareto_data.shape[0]-1):
+            S += (pareto_data[i,0] - pareto_data[i+1,0]) * (pareto_data[0,1] - pareto_data[i+1,1])
+        return S
+    
+
+df = pd.read_csv('./test.csv')
+Xtrain = df.iloc[:,:-2]
+Ytrain = df.iloc[:,-2:]
+
+mobo = Mobo4mat()
+
+mobo.fit(X = Xtrain, y = Ytrain, visual_data=Xtrain, method='HV',number= 1, objective='min', ref_point=[10, 10])
--- utils.py
+++ utils.py
@@ -32,61 +32,6 @@ def calculate_mean(list1, list2):
     result = [(x + y) / 2 for x, y in zip(list1, list2)]
     return result
 
-def non_dominated_sorting(fitness_values):
-    num_objectives = fitness_values.shape[1]
-    num_solutions = fitness_values.shape[0]
-
-    domination_counts = np.zeros(num_solutions, dtype=int)
-    dominated_solutions = [[] for _ in range(num_solutions)]
-    frontiers = [[]]
-
-    for i in range(num_solutions):
-        for j in range(i + 1, num_solutions):
-            if np.all(fitness_values[i] <= fitness_values[j]):
-                if np.any(fitness_values[i] < fitness_values[j]):
-                    domination_counts[j] += 1
-                else:
-                    dominated_solutions[i].append(j)
-            elif np.all(fitness_values[i] >= fitness_values[j]):
-                if np.any(fitness_values[i] > fitness_values[j]):
-                    domination_counts[i] += 1
-                else:
-                    dominated_solutions[j].append(i)
-
-        if domination_counts[i] == 0:
-            frontiers[0].append(i)
-
-    i = 0
-    while len(frontiers[i]) > 0:
-        next_frontier = []
-        for j in frontiers[i]:
-            for k in dominated_solutions[j]:
-                domination_counts[k] -= 1
-                if domination_counts[k] == 0:
-                    next_frontier.append(k)
-        i += 1
-        frontiers.append(next_frontier)
-
-    return frontiers[:-1]
-
-def find_non_dominated_solutions(fitness_values, **kargs):
-    fronts = non_dominated_sorting(fitness_values)
-    non_dominated_solutions_idx = []
-    for front in fronts:
-        non_dominated_solutions_idx.extend(front)
-    non_dominated_solutions_Data = fitness_values[non_dominated_solutions_idx]
-    non_dominated_solutions_Data = pd.DataFrame(non_dominated_solutions_Data, columns=feature_name)
-    non_dominated_solutions_Data.sort_values(by=feature_name[0], inplace=True)
-
-    return  non_dominated_solutions_Data
-
-def dominated_hypervolume(pareto_data, ref_point):
-    pareto_data = np.vstack([pareto_data, ref_point])
-    pareto_data = pareto_data[np.argsort(-pareto_data[:,0])]
-    S = 0
-    for i in range(pareto_data.shape[0]-1):
-        S += (pareto_data[i,0] - pareto_data[i+1,0]) * (pareto_data[0,1] - pareto_data[i+1,1])
-    return S
 
 def plot(pareto_front, targets, target_names):
     fig, ax = plt.subplots()
